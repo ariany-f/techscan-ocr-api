df0624fc212c4ccd2a3258307e17c4bbee0e631b
ariany-f <ariany_f@hotmail.com>
2024-02-07 16:44:57 -0300

    Ajuste

--- web/src/Controller/Component/SecurOSComponent.php
+++ web/src/Controller/Component/SecurOSComponent.php
@@ -20,6 +20,8 @@ namespace Src\Controller\Component {
          * 
          */
         private $service;
+        private $token;
+        private $public;
 
         /**
          * BiComponent constructor
