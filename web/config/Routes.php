a304a6ccbc4ef5a6c5dfd830a3c4e3e8c42d7ebe
ariany-f <ariany_f@hotmail.com>
2024-02-13 16:37:57 -0300

    Ajuste rota

--- web/config/Routes.php
+++ web/config/Routes.php
@@ -102,7 +102,7 @@ namespace Config {
                 'action' => 'subscriptions',
                 'pass' => []
             ],
-            'tempo-vinculo/*' => [
+            'tempo-vinculo-passagens/*' => [
                 'controller' => 'Index',
                 'action' => 'tempo',
                 'pass' => []
