10d3a3c273a4a1cd5bb190012c91fec1376ba7c3
ariany-f <ariany_f@hotmail.com>
2024-02-16 11:35:21 -0300

    Ajuste virgulas

--- web/src/Model/Passage/PassageModel.php
+++ web/src/Model/Passage/PassageModel.php
@@ -102,7 +102,7 @@ namespace src\Model\Passage {
                 $where .= (!empty($data_final)) ? " AND passages.datetime <= '$data_final'" : "";
                 $sql = "
                 SELECT 
-                    GROUP_CONCAT(dt.id, ' | ') as id,
+                    GROUP_CONCAT(dt.id, '') as id,
                     GROUP_CONCAT(dt.updated_by, ' | ') as updated_by,
                     GROUP_CONCAT(dt.updated_at, ' | ') as updated_at,
                     GROUP_CONCAT(dt.is_ok, ' | ') as status,
@@ -112,8 +112,8 @@ namespace src\Model\Passage {
                     GROUP_CONCAT(dt.container, '') as container,
                     dt.direction,
                     dt.gate,
-                    GROUP_CONCAT(dt.camera, ' | ') as cameras,
-                    GROUP_CONCAT(dt.images, ',') as images
+                    GROUP_CONCAT(dt.camera, ', ') as cameras,
+                    GROUP_CONCAT(dt.images, ', ') as images
             FROM
                 (
                     SELECT 
