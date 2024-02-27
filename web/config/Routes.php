455343c521cee0dfa5b602841640445efcfe47f9
ariany-f <ariany_f@hotmail.com>
2024-02-27 13:13:39 -0300

    ajuste rota

--- web/config/Routes.php
+++ web/config/Routes.php
@@ -43,7 +43,7 @@ namespace Config {
                 'pass' => []
             ],
             'ocr-passagens/*' => [
-                'controller' => 'GetOcrData',
+                'controller' => 'GetOCRData',
                 'action' => 'passagens',
                 'pass' => []
             ],
