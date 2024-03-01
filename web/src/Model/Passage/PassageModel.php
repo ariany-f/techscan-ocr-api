2a9a1080c51687e1470cd787d0b30c369c52130f
ariany-f <ariany_f@hotmail.com>
2024-03-01 11:15:22 -0300

    ajuste

--- web/src/Model/Passage/PassageModel.php
+++ web/src/Model/Passage/PassageModel.php
@@ -59,7 +59,7 @@ namespace src\Model\Passage {
                         cameras.position AS Camera,
                         representative_img.url AS Posicao,
                         sum(case when preset_reason IS NULL AND description_reason IS NULL then 1 else 0 end) AS Acertos,
-                        sum(case when preset_reason IS NOT NULL OR description_reason IS NOT NULL then 1 else 0 end) AS Erros
+                        sum(case when preset_reason IS NOT NULL OR description_reason IS NOT NULL OR updated_at IS NOT NULL then 1 else 0 end) AS Erros
                     FROM passages
                         INNER JOIN cameras ON cameras.id = passages.camera
                         INNER JOIN representative_img ON representative_img.id = cameras.representative_img_id
