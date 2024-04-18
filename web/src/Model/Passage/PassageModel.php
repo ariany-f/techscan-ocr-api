2e35bbfa7d1896eea31d0967066450ff751a2d11
ariany-f <ariany_f@hotmail.com>
2024-04-18 18:35:39 -0300

    teste

--- web/src/Model/Passage/PassageModel.php
+++ web/src/Model/Passage/PassageModel.php
@@ -113,7 +113,7 @@ namespace src\Model\Passage {
                                     WHEN users.name THEN 'Erro'
                                     ELSE 'Aprovada'
                                 END
-                            WHEN users.name THEN 'Erro'
+                            WHEN users.name IS NOT NULL THEN 'Erro'
                             ELSE 'Pendente'
                         END as status
                      FROM passage_bind
